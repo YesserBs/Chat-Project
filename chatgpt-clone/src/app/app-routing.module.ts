19883b8c22d0671d9f74154477cba7b8f37f8dce
Yesser <benselmayasser1@gmail.com>
2026-03-26 22:04:16 +0100

    I started with the side bar (the most catchy)

--- chatgpt-clone/src/app/app-routing.module.ts
+++ chatgpt-clone/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SidebarComponent } from './compos/sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: HomeComponent },
+  { path: '', component: SidebarComponent },       
+  { path: '**', redirectTo: '' }                          
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- chatgpt-clone/src/app/app.module.ts
+++ chatgpt-clone/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './compos/sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
